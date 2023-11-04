c1e8c529162726db4e20bb37d8040cf79f2c1677
Pratham Choudhary <prathamchoudhary23@gmail.com>
2023-11-04 23:32:55 +0530

    Added Header component

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
